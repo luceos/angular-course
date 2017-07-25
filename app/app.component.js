fada7ddf3739c48bc84a94c30210a0a280f58907
Daniël Klabbers <daniel@klabbers.email>
2017-07-25 21:31:57 +0200

    some changes

--- app/app.component.js
+++ app/app.component.js
@@ -5,19 +5,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var product_service_1 = require("./products/product.service");
+var router_1 = require("@angular/router");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(router) {
+        this.router = router;
         this.pageTitle = 'Pony Regulatory Gravity';
     }
+    AppComponent.prototype.isActive = function (url, exact) {
+        return this.router.isActive(url, exact ? exact : true);
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'pm-app',
             providers: [product_service_1.ProductService],
-            template: "\n        <div>\n            <nav class=\"navbar navbar-default\">\n                <div class=\"container\">\n                    <a class=\"navbar-brand\">{{ pageTitle }}</a>\n                    <ul class=\"nav navbar-nav\">\n                        <li><a [routerLink]=\"['/welcome']\"><i class=\"fa fa-home\"></i></a></li>\n                        <li><a [routerLink]=\"['/products']\"><i class=\"fa fa-archive\"></i></a></li>\n                    </ul>\n                </div>\n            </nav>\n            <div class=\"container\">\n                <router-outlet></router-outlet>\n            </div>\n        </div>\n    "
-        })
+            template: "\n        <div>\n            <nav class=\"navbar navbar-default navbar-fixed-top\">\n                <div class=\"container\">\n                    <a class=\"navbar-brand\">{{ pageTitle }}</a>\n                    <ul class=\"nav navbar-nav\">\n                        <li [ngClass]=\"{'active': isActive('/welcome')}\"><a [routerLink]=\"['/welcome']\">\n                            <i class=\"fa fa-home\"></i> home</a>\n                        </li>\n                        <li [ngClass]=\"{'active': isActive('/products') || isActive('/product/', false)}\">\n                            <a [routerLink]=\"['/products']\"><i class=\"fa fa-archive\"></i> products</a>\n                        </li>\n                    </ul>\n                </div>\n            </nav>\n            <div class=\"container\">\n                <router-outlet></router-outlet>\n            </div>\n        </div>\n    "
+        }),
+        __metadata("design:paramtypes", [router_1.Router])
     ], AppComponent);
     return AppComponent;
 }());
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ProductService} from './products/product.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'pm-app',
     providers: [ProductService],
     template: `
         <div>
-            <nav class="navbar navbar-default">
+            <nav class="navbar navbar-default navbar-fixed-top">
                 <div class="container">
                     <a class="navbar-brand">{{ pageTitle }}</a>
                     <ul class="nav navbar-nav">
-                        <li><a [routerLink]="['/welcome']"><i class="fa fa-home"></i></a></li>
-                        <li><a [routerLink]="['/products']"><i class="fa fa-archive"></i></a></li>
+                        <li [ngClass]="{'active': isActive('/welcome')}"><a [routerLink]="['/welcome']">
+                            <i class="fa fa-home"></i> home</a>
+                        </li>
+                        <li [ngClass]="{'active': isActive('/products') || isActive('/product/', false)}">
+                            <a [routerLink]="['/products']"><i class="fa fa-archive"></i> products</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
@@ -24,4 +29,11 @@ import {ProductService} from './products/product.service';
 
 export class AppComponent {
     pageTitle: string = 'Pony Regulatory Gravity';
+
+    constructor(private router: Router) {
+    }
+
+    isActive(url: string, exact?: boolean): boolean {
+        return this.router.isActive(url, exact ? exact : true);
+    }
 }
